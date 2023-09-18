ebfc53e5c903d0785e2c50a7de7495ada7d77e50
Ron David <rondavid209@gmail.com>
2023-09-18 20:41:59 +0300

    getline, history and memory

---
+++ help.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+
+/**
+ * display_help - Displaying help for Builtin
+ * @cmd: Parsed Command
+ * @er: Statue of Last Command Executed
+ * Return: 0 Success -1 fail
+ */
+int display_help(char **cmd, __attribute__((unused))int er)
+{
+	int fd, fw, rd = 1;
+	char c;
+
+	fd = open(cmd[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error");
+		return (0);
+	}
+
+	while (rd > 0)
+	{
+		rd = read(fd, &c, 1);
+		fw = write(STDOUT_FILENO, &c, rd);
+		if (fw < 0)
+		{
+			return (-1);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
+/**
+ * echo_bul - Execute Echo Cases
+ * @st: Statue of Last Commant Executed
+ * @cmd: Parsed Command
+ * Return: Always 0 or Execute Normal Echo
+ */
+int echo_bul(char **cmd, int st)
+{
+	char *path;
+	unsigned int pid = getppid();
+
+	if (_strncmp(cmd[1], "$?", 2) == 0)
+	{
+		print_number_in(st);
+		PRINTER("\n");
+	}
+	else if (_strncmp(cmd[1], "$$", 2) == 0)
+	{
+		print_number(pid);
+		PRINTER("\n");
+	}
+	else if (_strncmp(cmd[1], "$PATH", 5) == 0)
+	{
+		path = _getenv("PATH");
+		PRINTER(path);
+		PRINTER("\n");
+		free(path);
+
+	}
+	else
+		return (print_echo(cmd));
+
+	return (1);
+}
+
+
